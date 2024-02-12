fa5da157210480db43a96a2e2b1f96ae0e9227b6
aakashbhardwaj08 <aakash.bhardwaj@iiitb.ac.in>
2024-02-12 10:08:06 +0530

    changes made

--- gitSCM.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-# A simple calculator shell program
-
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Division"
-
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#This is Python Program to print hello
+
+print("hello World,Git Demo....feb2024_update\n")
+
+
